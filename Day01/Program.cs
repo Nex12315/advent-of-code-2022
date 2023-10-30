d9e13bf058a7e4dec975a6acdce24088d1a41654
Nex12315 <nikita20021804@gmail.com>
2023-10-30 14:07:07 +0200

    the worst solution to day 01 is done

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,3 +1,67 @@
-﻿using System;
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
-System.Console.WriteLine("xyi");
+string[] lines = File.ReadAllLines("./input.in");
+
+List<int> calories = new List<int> { };
+
+System.Console.WriteLine(lines);
+
+int sum = 0;
+int amount = 0;
+
+foreach (var line in lines)
+{
+    if (line != "")
+    {
+        sum += int.Parse(line);
+    }
+    else
+    {
+        calories.Add(sum);
+        sum = 0;
+        amount++;
+    }
+}
+
+int max1 = 0;
+int max2 = 0;
+int max3 = 0;
+
+int chosen = 0;
+
+foreach (int number in calories)
+{
+    if (max1 < number)
+    {
+        max1 = number;
+        chosen = number;
+    }
+}
+
+calories.Remove(chosen);
+
+foreach (int number in calories)
+{
+    if (max2 < number)
+    {
+        max2 = number;
+        chosen = number;
+    }
+}
+
+calories.Remove(chosen);
+
+foreach (int number in calories)
+{
+    if (max3 < number)
+    {
+        max3 = number;
+        chosen = number;
+    }
+}
+
+calories.Remove(chosen);
+
+System.Console.WriteLine(max1 + max2 + max3);
