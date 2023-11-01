db149c1684698a2779654e7472f02e2ed5ada525
Nex12315 <nikita20021804@gmail.com>
2023-11-01 14:27:09 +0200

    added solution for day2 part2

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.IO;
+using System;
 
 string[] lines = File.ReadAllLines("./input.in");
+// string[] lines = File.ReadAllLines("./test_input.in");
 
 int score = 0;
 
@@ -10,7 +12,17 @@ foreach (var line in lines)
     score += addScore(me) + winLose(opponent, me);
 }
 
-System.Console.WriteLine(score);
+System.Console.WriteLine("Part 1 answer: " + score);
+
+score = 0;
+
+foreach (var line in lines)
+{
+    var (opponent, me) = convert2(line);
+    score += addScore(me) + winLose(opponent, me);
+}
+
+System.Console.WriteLine("Part 2 answer: " + score);
 
 static int winLose(string opponent, string me)
 {
@@ -102,3 +114,74 @@ static (string Opponent, string Me) convert(string line)
     return (Opponent: opponent, Me: me);
 }
 
+static (string opponent, string me) convert2(string line)
+{
+    var parts = line.Split(' ');
+    var (opponent, me) = (parts[0], parts[1]);
+    Option result;
+
+    switch (opponent)
+    {
+        case "A":
+            opponent = "Rock";
+            result = new Rock();
+            break;
+        case "B":
+            opponent = "Paper";
+            result = new Paper();
+            break;
+        case "C":
+            opponent = "Scissors";
+            result = new Scissors();
+            break;
+        default:
+            throw new InvalidOperationException($"Invalid opponent value: {opponent}");
+    }
+
+    switch (me)
+    {
+        case "X":
+            me = result.Lose;
+            break;
+        case "Y":
+            me = result.Draw;
+            break;
+        case "Z":
+            me = result.Win;
+            break;
+    }
+
+    return (opponent: opponent, me: me);
+}
+
+interface Option
+{
+    string Name { get; }
+    string Win { get; }
+    string Draw { get; }
+    string Lose { get; }
+}
+
+class Rock : Option
+{
+    public string Name => "Rock";
+    public string Win => "Paper";
+    public string Draw => "Rock";
+    public string Lose => "Scissors";
+}
+
+class Paper : Option
+{
+    public string Name => "Paper";
+    public string Win => "Scissors";
+    public string Draw => "Paper";
+    public string Lose => "Rock";
+}
+
+class Scissors : Option
+{
+    public string Name => "Scissors";
+    public string Win => "Rock";
+    public string Draw => "Scissors";
+    public string Lose => "Paper";
+}
